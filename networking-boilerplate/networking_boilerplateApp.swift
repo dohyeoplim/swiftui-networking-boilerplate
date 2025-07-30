90a4048668ac10491edbee7dcf79b5908e1b3d3f
Dohyeop Lim <dhlim0817@gmail.com>
2025-07-30 15:30:45 +0900

    Initial Commit

---
+++ networking-boilerplate/networking_boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  networking_boilerplateApp.swift
+//  networking-boilerplate
+//
+//  Created by dohyeoplim on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct networking_boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
